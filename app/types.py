ede3a31eecb6fc13b0fba0e08512f6cf382ed297
ung nguyen <commitment1511@outlook.com>
2023-08-01 23:57:51 +0700

    query array field

--- app/types.py
+++ app/types.py
@@ -1,19 +1,22 @@
-from typing import List
 import strawberry
-
+from app.models import Card, List
 
 @strawberry.type
 class TableType:
     id: int
     title: str
-    lists:List['ListType']
+    @strawberry.field
+    def lists(self) -> list['ListType']:
+        return List.objects.filter(table_id=self.id)
 
 @strawberry.type
 class ListType:
     id: int
     title: str
     table:TableType
-    cards:List['CardType']
+    @strawberry.field
+    def cards(self) -> list['CardType']:
+        return Card.objects.filter(list_id=self.id)
 
 @strawberry.type
 class CardType:
